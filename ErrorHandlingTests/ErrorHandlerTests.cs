55bea7b6b672205944e9a28e05845a281c72a6bc
Marcos Venicius <marcosdevelopment.business@gmail.com>
2022-12-17 10:48:14 -0300

    test: add missing test for ErrorHandler

--- ErrorHandlingTests/ErrorHandlerTests.cs
+++ ErrorHandlingTests/ErrorHandlerTests.cs
@@ -20,4 +20,29 @@ public class ErrorHandlerTests
 
         Assert.Single(exception.Errors);
     }
+
+    [Fact]
+    public void Should_Throw_All_Errors_When_Distinct_Is_Disabled()
+    {
+        var exception = Assert.ThrowsAny<ErrorHandlerBaseException>(() =>
+        {
+            using (var errorHandler = new ErrorHandler())
+            {
+                errorHandler.Add(new("error", "first error"));
+                errorHandler.Add(new("error", "second error"));
+            }
+        });
+
+        Assert.Equal(2, exception.Errors.Count());
+    }
+
+    [Fact]
+    public void Should_Not_Throw_Exception_If_No_Has_Errors()
+    {
+        var errorHandler = new ErrorHandler();
+
+        errorHandler.Dispose();
+
+        Assert.Empty(errorHandler.Errors);
+    }
 }
